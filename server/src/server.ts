4ece024bc4e63e85f94ec19695b87e288f69b87e
Rafael Pianaro <rafaelpianaro@gmail.com>
2022-10-30 17:54:05 -0300

    server - db sqlite

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,21 @@
 // maneira antiga de importacao
 // const express = require('express')
 // ECMAScript Modules foi implementado
-import express from "express";
+import express, { response } from "express";
 
 const app = express()
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+    return response.json([])
+})
+
+app.post('/ads', (request, response) => {
+    return response.status(201).json([])
+})
+
+app.get('/games/:id/ads', (request, response) => {
+    // const gameId = request.params.id
+    
     return response.json([
         { id: 1, name: 'Anúncio 1' },
         { id: 2, name: 'Anúncio 2' },
@@ -13,6 +23,12 @@ app.get('/ads', (request, response) => {
         { id: 4, name: 'Anúncio 4' },
         { id: 5, name: 'Anúncio 5' },
     ])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+    // const adId = request.params.id
+    
+    return response.json([])
 })
 
 app.listen(3333)
